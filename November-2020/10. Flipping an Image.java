4592784609f1c73b0217a949949e898860c48a8d
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-11-11 09:38:12 +0530

    Create 10. Flipping an Image.java

---
+++ November-2020/10. Flipping an Image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int r = A.length;
+        int c = A.length;
+        
+        int[][] res = new int[r][c];
+        
+        for(int i = 0; i < r; i++) {
+            int k = 0;
+            for(int j = c - 1; j >= 0; j--) {
+                if(A[i][j] == 1)
+                    res[i][k++] = 0;
+                else 
+                    res[i][k++] = 1;
+                
+            }
+        }
+        
+        return res;
+    }
+}
